574649d3c7ff12ad59ca6bb02ce0f8f4ca85cc6f
Rishav Raj <rishav.24b1541080@abes.ac.in>
2025-11-27 16:23:17 +0530

    Security Fixes

--- firebase-config.js
+++ firebase-config.js
@@ -1,12 +1,12 @@
 // Firebase CDN App + Firestore SDK (Browser Compatible)
 const firebaseConfig = {
-    apiKey: "AIzaSyCUP1poCwCrCh7L0K75bSpNILUnIvLxmNQ",
-    authDomain: "trust-note.firebaseapp.com",
-    projectId: "trust-note",
-    storageBucket: "trust-note.firebasestorage.app",
+    apiKey: "YOUR-API-KEY",
+    authDomain: "XXXXXXXXXXXX.firebaseapp.com",
+    projectId: "YOUR-NAME",
+    storageBucket: "YOUR-NAME.firebasestorage.app",
     messagingSenderId: "438221738438",
-    appId: "1:438221738438:web:78845f57cc7ac9edc462c7",
-    measurementId: "G-4B37J09FVW"
+    appId: "XXXXXXXXX",
+    measurementId: "XXXXXXXXXXX"
 };
 
 // Initialize Firebase (Browser Version)
